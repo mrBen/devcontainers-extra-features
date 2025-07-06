2e154fb74ce9e6e1b96f22d3bb56d0820eba86de
koralowiec <36413794+koralowiec@users.noreply.github.com>
2025-07-06 12:56:33 +0000

    fix(argocd): use gh-release feat to install

--- src/argo-cd/install.sh
+++ src/argo-cd/install.sh
@@ -1,87 +1,22 @@
 #!/usr/bin/env bash
 
-ARGOCD_VERSION="${VERSION:-"latest"}"
-
 set -e
 
-# Clean up
-rm -rf /var/lib/apt/lists/*
+source ./library_scripts.sh
 
-architecture="$(uname -m)"
-case ${architecture} in
-x86_64) architecture="amd64" ;;
-aarch64 | armv8*) architecture="arm64" ;;
-aarch32 | armv7* | armvhf*) architecture="arm" ;;
-i?86) architecture="386" ;;
-*)
-	echo "(!) Architecture ${architecture} unsupported"
-	exit 1
-	;;
-esac
+# nanolayer is a cli utility which keeps container layers as small as possible
+# source code: https://github.com/devcontainers-extra/nanolayer
+# `ensure_nanolayer` is a bash function that will find any existing nanolayer installations,
+# and if missing - will download a temporary copy that automatically get deleted at the end
+# of the script
+ensure_nanolayer nanolayer_location "v0.5.6"
 
-if [ "$(id -u)" -ne 0 ]; then
-	echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
-	exit 1
-fi
+# Example nanolayer installation via devcontainer-feature
+$nanolayer_location \
+    install \
+    devcontainer-feature \
+    "ghcr.io/devcontainers-extra/features/gh-release:1" \
+    --option repo='argoproj/argo-cd' --option binaryNames='argocd' \
+    --option version="$VERSION" --option releaseTagRegex='^(?!.*(alpha|beta|rc)).*$'
 
-# Checks if packages are installed and installs them if not
-check_packages() {
-	if ! dpkg -s "$@" >/dev/null 2>&1; then
-		if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
-			echo "Running apt-get update..."
-			apt-get update -y
-		fi
-		apt-get -y install --no-install-recommends "$@"
-	fi
-}
-
-# Figure out correct version of a three part version number is not passed
-find_version_from_git_tags() {
-	local variable_name=$1
-	local requested_version=${!variable_name}
-	if [ "${requested_version}" = "none" ]; then return; fi
-	local repository=$2
-	local prefix=${3:-"tags/v"}
-	local separator=${4:-"."}
-	local last_part_optional=${5:-"false"}
-	if [ "$(echo "${requested_version}" | grep -o "." | wc -l)" != "2" ]; then
-		local escaped_separator=${separator//./\\.}
-		local last_part
-		if [ "${last_part_optional}" = "true" ]; then
-			last_part="(${escaped_separator}[0-9]+)?"
-		else
-			last_part="${escaped_separator}[0-9]+"
-		fi
-		local regex="${prefix}\\K[0-9]+${escaped_separator}[0-9]+${last_part}$"
-		local version_list="$(git ls-remote --tags ${repository} | grep -oP "${regex}" | tr -d ' ' | tr "${separator}" "." | sort -rV)"
-		if [ "${requested_version}" = "latest" ] || [ "${requested_version}" = "current" ] || [ "${requested_version}" = "lts" ]; then
-			declare -g ${variable_name}="$(echo "${version_list}" | head -n 1)"
-		else
-			set +e
-			declare -g ${variable_name}="$(echo "${version_list}" | grep -E -m 1 "^${requested_version//./\\.}([\\.\\s]|$)")"
-			set -e
-		fi
-	fi
-	if [ -z "${!variable_name}" ] || ! echo "${version_list}" | grep "^${!variable_name//./\\.}$" >/dev/null 2>&1; then
-		echo -e "Invalid ${variable_name} value: ${requested_version}\nValid values:\n${version_list}" >&2
-		exit 1
-	fi
-	echo "${variable_name}=${!variable_name}"
-}
-
-# make sure we have curl
-check_packages ca-certificates curl
-
-# make sure version is available
-find_version_from_git_tags ARGOCD_VERSION 'https://github.com/argoproj/argo-cd'
-
-# download and install binary
-ARGOCD_FILENAME=argocd-linux-${architecture}
-curl -sSL https://github.com/argoproj/argo-cd/releases/download/v${ARGOCD_VERSION}/${ARGOCD_FILENAME} -o argocd-linux
-install -m 555 argocd-linux /usr/local/bin/argocd
-rm argocd-linux
-
-# Clean up
-rm -rf /var/lib/apt/lists/*
-
-echo "Done!"
+echo 'Done!'
